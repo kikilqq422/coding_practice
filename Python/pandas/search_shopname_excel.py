d7b8cbce5e3ac9b37dddde54044c68f6d1d92677
kikilqq422 <72486608+kikilqq422@users.noreply.github.com>
2022-05-18 22:24:19 +0800

    Update search_shopname_excel.py
    change file name

--- Python/pandas/search_shopname_excel.py
+++ Python/pandas/search_shopname_excel.py
@@ -2,13 +2,13 @@ import numpy_prac as np
 import pandas as pd
 from pandas import Series,DataFrame
 
-df_shop_id=pd.read_excel(r"F://峥呈//metabase数据11.11//5-二级商户号.xlsx")
-df_data = pd.read_excel(r"F://峥呈//metabase数据11.11//重录汇总9.03.xlsx")
+df_shop_id=pd.read_excel(r"shopids.xlsx")
+df_data = pd.read_excel(r"shop_messages.xlsx")
 index_l = []
-for id in df_shop_id["二级商户号"]:
+for id in df_shop_id["shop_name"]:
     for index in df_data.index:
         if (df_data.iloc[index].isin([id])).any():
             index_l.append(index)
 df_p = df_data.iloc[sorted(list(set(index_l)))]
 
-df_p.to_excel(r'F://峥呈//metabase数据11.11//整理.xlsx')
+df_p.to_excel(r'result.xlsx')
